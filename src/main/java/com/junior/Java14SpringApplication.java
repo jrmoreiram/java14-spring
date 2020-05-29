c9a3738c9019baeced4d957a3865237488891f44
Júnior Moreira Martins <jumoreiram@gmail.com>
2020-05-28 23:19:22 -0300

    Commit de criação do projeto.

---
+++ src/main/java/com/junior/Java14SpringApplication.java
@@ -0,0 +1,13 @@
+package com.junior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java14SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java14SpringApplication.class, args);
+	}
+
+}
