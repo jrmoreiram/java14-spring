f1a66e6aa03f0bb8c275c5b206a01dd49de57fb4
Júnior Moreira Martins <jumoreiram@gmail.com>
2020-05-29 22:42:41 -0300

    Commit criação de um controller uma entidade um repositório e configuração da conexão com banco.

---
+++ src/main/java/com/junior/Product.java
@@ -0,0 +1,25 @@
+package com.junior;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String  name;
+
+    private int status;
+}
---
+++ src/main/java/com/junior/ProductController.java
@@ -0,0 +1,23 @@
+package com.junior;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductRepository repository;
+
+    public ProductController(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+}
